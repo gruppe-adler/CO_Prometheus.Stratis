8e1d8d6061cd7064d93221c2e311d451f5147fa8
nomisum <nomisum@googlemail.com>
2016-08-05 23:07:45 +0200

    Update CSSA3_header.hpp

--- CSSA3/CSSA3_header.hpp
+++ CSSA3/CSSA3_header.hpp
@@ -3,6 +3,7 @@
 respawn = 3;
 respawnDialog = 0;
 respawnTemplatesWest[] = {"MenuPosition"};
+respawnDelay = 30;	//seconds
 
 #include "dialogs\basic_defines.hpp"
 #include "dialogs\CSSA3_defines.hpp"
